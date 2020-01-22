be5e0018025a3d663e24840f981b99d46b70dc27
VivekG86 <vivek_g86@yahoo.com>
2020-01-22 16:41:36 -0500

    Initial code checkin
    Initial code checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftbodyComponent } from './leftbody/leftbody.component';
+import { RightbodyComponent } from './rightbody/rightbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftbodyComponent,
+    RightbodyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rightbody/rightbody.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rightbody',
+  templateUrl: './rightbody.component.html',
+  styleUrls: ['./rightbody.component.sass']
+})
+export class RightbodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
