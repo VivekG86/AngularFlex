9682fa09668023152ea6f3ca3aa4820b84258c0a
VivekG86 <vivek_g86@yahoo.com>
2020-01-23 03:42:48 -0500

    Modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LeftbodyComponent } from './leftbody/leftbody.component';
-import { RightbodyComponent } from './rightbody/rightbody.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
+import { BodyComponent } from './layout/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
     HeaderComponent,
-    FooterComponent,
-    LeftbodyComponent,
-    RightbodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RightbodyComponent } from './rightbody.component';
+import { LayoutComponent } from './layout.component';
 
-describe('RightbodyComponent', () => {
-  let component: RightbodyComponent;
-  let fixture: ComponentFixture<RightbodyComponent>;
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RightbodyComponent ]
+      declarations: [ LayoutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RightbodyComponent);
+    fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
